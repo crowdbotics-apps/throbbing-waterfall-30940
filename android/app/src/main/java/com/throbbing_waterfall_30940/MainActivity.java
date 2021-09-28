0e7bf2c123076dd58f3f9a79ac63f167bc940e36
Crowdbotics <team@crowdbotics.com>
2021-09-28 22:26:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/throbbing_waterfall_30940/MainActivity.java
@@ -0,0 +1,15 @@
+package com.throbbing_waterfall_30940;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "throbbing_waterfall_30940";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "throbbing_waterfall_30940",
+  url: "https://throbbing_waterfall_30940.botics.co",
+  api: "https://throbbing_waterfall_30940.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
